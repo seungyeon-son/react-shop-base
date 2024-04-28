de07b8fe7e46dff755e7165150b968657330dd00
syson <au1gust8@gmail.com>
2024-04-29 00:33:24 +0900

    ItemList 작업 중

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -1,17 +1,20 @@
 import Slider from "../components/common/Slider";
+import ItemList from "../components/products/ItemList";
 
 const Index = (): JSX.Element => {
   return (
     <>
       <Slider />
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mt-10 xl:container mx-auto">
-         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
+        <ItemList readonly={undefined} />
+        {/* TODO: 아이템리스트 카테고리 분기, 링크 적용, 노출할 개수 설정 필요 */}
+        {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
       </section>
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 xl:container mx-auto">
-         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
+        {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
       </section>
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mb-20 xl:container mx-auto">
-         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
+        {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
       </section>
     </>
   );
