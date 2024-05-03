7eafd574c3abf0b8fda907392920dbe22452d552
syson <au1gust8@gmail.com>
2024-05-04 02:51:15 +0900

    index 레이아웃 적용

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -6,14 +6,18 @@ const Index = (): JSX.Element => {
     <>
       <Slider />
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mt-10 xl:container mx-auto">
-        <ItemList readonly={undefined} />
-        {/* TODO: 아이템리스트 카테고리 분기, 링크 적용, 노출할 개수 설정 필요 */}
+        <h2 className="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">패션</h2>
+        <ItemList />
+
         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
       </section>
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 xl:container mx-auto">
+        <h2 className="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">악세서리</h2>
         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
       </section>
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mb-20 xl:container mx-auto">
+        <h2 className="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">디지털</h2>
+
         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
       </section>
     </>
