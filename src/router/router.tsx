72cab70ff81c2e0d8c0c489fa7aae1a8bf3f0401
syson <au1gust8@gmail.com>
2024-05-05 02:30:58 +0900

    라우팅 추가

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { memo } from "react";
 import Error from "../views/Error";
 import Index from "../views/Index";
+import Fashion from "../views/Fashion";
 
 const Router = (): JSX.Element => {
   return (
     <Routes>
       <Route path="*" element={<Error />} />
       <Route path="/" element={<Index />} />
+      <Route path="/fashion" element={<Fashion />} />
       {/* 라우팅 추가 해보세요. */}
     </Routes>
   );
