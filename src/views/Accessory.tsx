8c1afb440a3397ce96d401a8f06c2c4dfda86fdf
syson <au1gust8@gmail.com>
2024-05-06 15:32:34 +0900

    view page 생성

--- src/views/Accessory.tsx
+++ src/views/Accessory.tsx
@@ -5,20 +5,20 @@ import { MENUS } from "../constants/category";
  * 뷰페이지에는 특별한 로직이 포함되어서는 안됩니다.
  * 컴포넌트를 불러다 렌더링하는 용도로만 사용 하세요.
  */
-const Fashion = (): JSX.Element => {
+const Accessory = (): JSX.Element => {
   /**
    * 해당 부분에 함수나 기타 로직등을 작성하지마세요.
    */
   return (
     <section className="pt-4 lg:pt-5 pb-4 lg:pb-8 px-4 xl:px-2 xl:container mx-auto">
-      <BreadCrumb category={MENUS.HOME} crumb={MENUS.FASHION} />
-      <h1 className="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">패션</h1>
+      <BreadCrumb category={MENUS.HOME} crumb={MENUS.ACCESSORY} />
+      <h1 className="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">악세서리</h1>
       <article className="grid gap-6 md:grid-cols-2 lg:grid-cols-4 item_list">
         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
-        <ProductsLoad limit={10} Category={"jewelery"} />
+        <ProductsLoad limit={4} Category={"jewelery"} />
       </article>
     </section>
   );
 };
 
-export default Fashion;
+export default Accessory;
--- src/views/Digital.tsx
+++ src/views/Digital.tsx
@@ -1,24 +1,24 @@
 import BreadCrumb from "../components/common/Breadcrumb";
 import ProductsLoad from "../components/products/ProductsLoad";
-import { MENUS } from "../constants/category";
+import { Category, MENUS } from "../constants/category";
 /**
  * 뷰페이지에는 특별한 로직이 포함되어서는 안됩니다.
  * 컴포넌트를 불러다 렌더링하는 용도로만 사용 하세요.
  */
-const Fashion = (): JSX.Element => {
+const Digital = (): JSX.Element => {
   /**
    * 해당 부분에 함수나 기타 로직등을 작성하지마세요.
    */
   return (
     <section className="pt-4 lg:pt-5 pb-4 lg:pb-8 px-4 xl:px-2 xl:container mx-auto">
-      <BreadCrumb category={MENUS.HOME} crumb={MENUS.FASHION} />
-      <h1 className="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">패션</h1>
+      <BreadCrumb category={MENUS.HOME} crumb={MENUS.DIGITAL} />
+      <h1 className="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">디지털</h1>
       <article className="grid gap-6 md:grid-cols-2 lg:grid-cols-4 item_list">
         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
-        <ProductsLoad limit={10} Category={"jewelery"} />
+        <ProductsLoad limit={10} Category="electronics" key={"electronics"} />
       </article>
     </section>
   );
 };
 
-export default Fashion;
+export default Digital;
