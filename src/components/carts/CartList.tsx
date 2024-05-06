c869f956c0c98bffbb315687525dff3706a12845
syson <au1gust8@gmail.com>
2024-05-06 21:20:18 +0900

    cart 수정

--- src/components/carts/CartList.tsx
+++ src/components/carts/CartList.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { ICartState, cartState, removeFromCart } from "../../store/cart";
 import { toCurrencyFormat } from "../../helpers/helpers";
 import { useRecoilState } from "recoil";
+import CartView from "./CartView";
 
 const CartList = (): JSX.Element => {
   // Recoil을 사용해서 cart데이터를 가져오는 예제입니다.
@@ -15,9 +16,9 @@ const CartList = (): JSX.Element => {
   return (
     <div className="lg:flex lg:items-center mt-4 px-2 lg:px-0">
       {/* 카트 리스트 화면을 구성 해보세요. */}
+      <CartView />
     </div>
   );
 };
-
 
 export default CartList;
--- src/components/carts/CartView.tsx
+++ src/components/carts/CartView.tsx
@@ -4,11 +4,11 @@ import Confirm from "../common/Confirm";
 
 const CartView = (): JSX.Element => {
   return (
-    <>
+    <div className="flex flex-col px-4 w-full">
       <BreadCrumb category="홈" crumb="장바구니" />
-      <div className="mt-6 md:mt-14 px-2 lg:px-0">
+      <div className="mt-6 md:my-14 px-2 lg:px-0">
         {/* 물품이 없다면? */}
-        <div>
+        <div className="flex flex-col items-center justify-center h-[50vh]">
           <h1 className="text-2xl">장바구니에 물품이 없습니다.</h1>
           <Link to="/" className="btn btn-primary mt-10">
             담으러 가기
@@ -17,7 +17,7 @@ const CartView = (): JSX.Element => {
         {/* 구매하기 버튼 등 화면을 구성 해보세요. */}
       </div>
       <Confirm />
-    </>
+    </div>
   );
 };
 
